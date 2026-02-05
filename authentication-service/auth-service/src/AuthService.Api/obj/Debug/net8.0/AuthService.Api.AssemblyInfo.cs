776a1df82f1ed4551bbb610b5617787bfab8f92a
jhernandez2022 <jhernandez-2022010@kinal.edu.gt>
2026-02-05 14:13:12 -0600

    Carpetas con archivos que paso el proge

--- authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a13704a227abac772058a5636489d725b8e27de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+522e9dce7363587a3ba5bdfc7a546722fa3f97fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
