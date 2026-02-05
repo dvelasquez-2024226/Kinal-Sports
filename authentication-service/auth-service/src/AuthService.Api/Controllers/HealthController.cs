324e4b9028f3a123ba8bba80ab5d74ab13f35aef
Informatica <informatica@alumno.local>
2026-02-05 15:31:28 -0600

    Se creo un directorio en la capa Api llamado controler y se creo una clase llamada HealthController

---
+++ authentication-service/auth-service/src/AuthService.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class HealthController : ControllerBase
+{
+    public IActionResult GetHealth()
+    {
+        var response = new
+        {
+            status = "Healthy",
+            timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffz"),
+            service = "Kinal Sports AuthService"
+        }
+        return Ok(response);
+    }
+}
--- authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b78da77f0949f69548a6601d3bbcd72cf5e7fafc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e4c900f82e254ed5fa24714c7e8f7d86528572")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
