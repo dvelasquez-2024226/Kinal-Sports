8e8f50f462907689fffdb8b6cbd1ebcb48cae60a
Informatica <informatica@alumno.local>
2026-02-06 13:15:40 -0600

    Se agrego confuguracion en el appsettings

--- authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5fe1a4f7b42e5e60e8a9d9a45acc144db6a21c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3839f8218c16fd5893894e61b33f49e5eee91fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
