d212c85dd99e55b1a9134ef8116dfd94b9a3c0e3
Informatica <informatica@alumno.local>
2026-02-09 14:15:32 -0600

    Se creo la carpteta e inicio el proyecto de Server-Admon

--- authentication-service/auth-service/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2ccd8063cf1533a32f6ca07d360c110072d8c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e83ee216b0b0963f742a3aba4b91316fed1e3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
