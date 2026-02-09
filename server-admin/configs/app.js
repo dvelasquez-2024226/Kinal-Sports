35fc5e2cb8554437959039fbf987ce7690f7fc65
Informatica <informatica@alumno.local>
2026-02-09 15:52:29 -0600

    Se agrego la concexion funcional

--- server-admin/configs/app.js
+++ server-admin/configs/app.js
@@ -4,6 +4,9 @@ import express, { application } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import { dbConnection } from './db.js';
+
+const BASE_PATH = '/kinalSports/v1';
 
 const middlewares = (app) =>{
     app.use(express.urlencoded({extended: false, limit: '10mb'}));
@@ -13,13 +16,24 @@ const middlewares = (app) =>{
     app.use(helmet());
 };
 
-export const initServer = () => {
+const routes = (app) => {
+    app.use(`${BASE_PATH}/health`, (req, res) => {
+        res.status(200).json({
+            status: 'healthy',
+            status: 'Kinal Sports Admin Server'
+        })
+    })
+}
+
+export const initServer = async() => {
     const app =  express();
     const PORT = process.env.PORT;
     app.set('trust proxy', 1);
+    routes(app);
 
     try{
         middlewares(app);
+        await dbConnection();
 
         app.listen(PORT, () => {
             console.log(`Kinal Sports admin server running on port ${PORT}`)
