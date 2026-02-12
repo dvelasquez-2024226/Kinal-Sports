b47e9f89a64d2ba917a3d946b826cc3118567df1
Informatica <informatica@alumno.local>
2026-02-12 14:13:05 -0600

    se arreglo la base de middlewares

--- server-admin/src/fields/field.service.js
+++ server-admin/src/fields/field.service.js
@@ -1,2 +1,7 @@
 import Field from './field.model.js';
 
+ URI_MONGODB=mongodb://localhost:27017/admin-ks-in6av
+
+ CLOUDINARY_CLOUD_NAME=dmzpqylve
+ CLOUDINART_API_KEY=544624595956484
+ CLOUDINARY_API_SECRET=vEO5tzMIuiGAV9EYNMlF1eH3Eh4
