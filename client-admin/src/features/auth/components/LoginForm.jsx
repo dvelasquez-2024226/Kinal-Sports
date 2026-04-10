ec3f90ac53cdc8bea651e42ab0ba9054157f0eab
dvelasquez-2024226 <dvelasquez-2024226@kinal.edu.gt>
2026-04-10 15:35:19 -0600

    se agregaron vista de login y forgot password

--- client-admin/src/features/auth/components/LoginForm.jsx
+++ client-admin/src/features/auth/components/LoginForm.jsx
@@ -1,7 +1,60 @@
+import { useForm } from "react-hook-form"
 
-
-export const LoginForm = () => {
+export const LoginForm = ({onForgot}) => {
+  const {
+    register,
+    // hancdleSubmit,
+    formState: { errors }
+  } = useForm();
   return (
-    <div>Desde form de login</div>
+    <form className="space-y-5">
+      <div>
+        <label htmlFor="emailOrUsername" className="block text-sm font-medium text-gray-800 mb-1.5">
+          Email o Username
+        </label>
+        <input
+          type="text"
+          id="emailOrUsername"
+          placeholder="email@example.com o username"
+          className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+          {
+            ...register("emailOrUsername",{
+              required: "El email o username es requerido"
+            })
+          }
+        />
+        {errors.emailOrUsername && (
+          <p className="text-red-600 text-xs mt-1">{errors.emailOrUsername.message}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="password" className="block text-sm font-medium text-gray-800 mb-1.5">
+          Contraseña
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="********"
+          className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+          {
+            ...register("password",{
+              required: "La contraseña es requerida"
+            })
+          }
+
+        /> 
+        {errors.password && (
+          <p className="text-red-600 text-xs mt-1">{errors.password.message}</p>
+        )}
+      </div>
+      <button type="submit" className="w-full bg-main-blue hover:opacity-90 text-white font-medium py-2.5 px-4 rounded-lg transition-colors duration-200 text-sm">
+        Iniciar sesión
+      </button>
+      <p className="text-center text-sm">
+        <button type="button" onClick={onForgot} className="text-main-blue hover:underline hover:curson-pointer">
+          ¿Olvidaste tu contraseña?
+        </button>
+      </p>
+    </form>
   )
 }
